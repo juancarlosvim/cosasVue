f71fe5c700db1f953691475a1eb0a27ef5f5fd1d
juancarlosvim <juancarlosvillegasmarmol@gmail.com>
2019-03-23 12:12:19 +0100

    comunicacion entre componentes parte 2

--- comunicacionComponente/component/hijo.js
+++ comunicacionComponente/component/hijo.js
@@ -2,11 +2,19 @@ Vue.component('hijo', {
     template: //html
     `
     <div class="py-5 bg-success text-white">
-        <h4>Componente Hijo</h4>
-        <h5>{{numero}}</h5>
-       
+        <h4>Componente Hijo: {{numero}}</h4>
+        <h4>Nombre: {{nombre}}</h4>
+        <button @click="numero++">+</button>
     </div>
     `,
-    props: ['numero']
+    props: ['numero'],
+    data (){
+        return {
+            nombre: 'JuanCarlosVim'
+        }
+    },
+    mounted (){
+        this.$emit('nombreHijo', this.nombre);
+    },
     
 })
--- comunicacionComponente/component/padre.js
+++ comunicacionComponente/component/padre.js
@@ -5,12 +5,14 @@ Vue.component('padre', {
         <h2>Componente Padre: {{numeroPadre}}</h2>
         <button class="btn btn-danger" @click="numeroPadre++">+ </button>
         <button class="btn btn-info" @click="numeroPadre--">- </button>
-        <hijo :numero="numeroPadre"></hijo>
+        {{nombrePadre}}
+        <hijo :numero="numeroPadre" @nombreHijo="nombrePadre = $event"></hijo>
     </div>
     `,
     data (){
         return {
-            numeroPadre: 5
+            numeroPadre: 5,
+            nombrePadre: ''
         }
     }
 
