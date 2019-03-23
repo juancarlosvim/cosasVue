31080473d6eb595bfe66b7282b7b5d43b08b5684
juancarlosvim <juancarlosvillegasmarmol@gmail.com>
2019-03-23 09:39:27 +0100

    keyup, v-model y computed

--- v-model/js/main.js
+++ v-model/js/main.js
@@ -7,15 +7,26 @@ const app = new Vue({
             {nombre: 'Manzana', cantidad: 0},
             {nombre: 'Plátano', cantidad: 4},
         ],
-        nuevaFruta: ''
+        nuevaFruta: '',
+        total: 0
     },
     methods: {
         agregarFruta (){
             
             this.frutas.push({
                 nombre: this.nuevaFruta, cantidad: 0 
-            })
+            });
+            this.nuevaFruta = ''
         }
     },
+    computed: {
+        sumarFrutas(){
+            this.total = 0;
+            for(fruta of this.frutas){
+                this.total = this.total + fruta.cantidad;
+            }
+            return this.total;
+        }
+    }
    
 });
